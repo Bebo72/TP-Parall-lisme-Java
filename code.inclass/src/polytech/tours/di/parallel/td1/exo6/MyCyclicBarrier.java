ae475c15ea531d5c7c94b903d5c5f893a9a9c6be
Pierrick Bobet - Bebo <pierrick.bobet@cryptolab.net>
2017-03-24 17:10:00 +0100

    Ajout Exercice 6

--- code.inclass/src/polytech/tours/di/parallel/td1/exo6/MyCyclicBarrier.java
+++ code.inclass/src/polytech/tours/di/parallel/td1/exo6/MyCyclicBarrier.java
@@ -2,25 +2,57 @@ package polytech.tours.di.parallel.td1.exo6;
 
 public class MyCyclicBarrier implements CyclicBarrier {
 
-	public MyCyclicBarrier(int parties, Runnable barrierAction) {
-		//TODO TP1-EXO6: implement this constructor
-	}
+  private final int parties; // nombre de thread
+  private int count;
 
-	@Override
-	public void await() throws InterruptedException {
-		// TODO TP1-EXO6: implement this method
-	}
+  private Runnable action;
 
-	@Override
-	public int getNumberWaiting() {
-		// TODO TP1-EXO6: implement this method
-		return 0;
-	}
+  public MyCyclicBarrier(int parties) {
+    this(parties, null);
+  }
 
-	@Override
-	public int getParties() {
-		// TODO TP1-EXO6: implement this method
-		return 0;
-	}
+  public MyCyclicBarrier(int parties, Runnable barrierAction) {
+    // TODO TP1-EXO6: implement this constructor
+
+    if (parties <= 0) {
+      throw new IllegalStateException();
+    }
+
+    // Initialisations des attributs
+    this.parties = parties;
+    this.action = barrierAction;
+    this.count = this.parties;
+  }
+
+  @Override
+  public synchronized void await() throws InterruptedException {
+    // TODO TP1-EXO6: implement this method
+    this.count--;
+
+    if (this.count > 0) {
+      wait(); // pas le droit d'appeller wait si pas de synchro
+    }
+
+    else {
+      this.count = this.parties;
+
+      if (action != null) {
+        action.run();
+        notifyAll();
+      }
+    }
+  }
+
+  @Override
+  public int getNumberWaiting() {
+    // TODO TP1-EXO6: implement this method
+    return this.parties - this.count;
+  }
+
+  @Override
+  public int getParties() {
+    // TODO TP1-EXO6: implement this method
+    return this.parties;
+  }
 
 }
